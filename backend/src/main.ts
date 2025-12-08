15e488ba7dd16a7ccb3a2c250e92a33f8598dd4a
Rolymaya <rolitchutchu@gmail.com>
2025-12-08 09:01:45 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -38,6 +38,7 @@ async function bootstrap() {
     SwaggerModule.setup('api/docs', app, document);
 
     const port = process.env.PORT || 3001;
+    await app.listen(port, '0.0.0.0');
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
